ac27b59f9e2b6f413112c47ef0d04761bd888b82
mammalofski <masahati.navid@gmail.com>
2018-08-20 09:20:36 +0430

    add static files

--- facego/facego/urls.py
+++ facego/facego/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
@@ -22,3 +23,5 @@ urlpatterns = [
     path('fa/', include('faceapp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
