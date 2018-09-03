d27af14fe54b6307a7892b6774cbcf2e17d0d4d3
mammalofski <masahati.navid@gmail.com>
2018-09-03 10:54:28 +0430

    advanced prefetch related

--- facego/faceapp/views.py
+++ facego/faceapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView
 from django.http import Http404
-from django.db.models import Count
+from django.db.models import Count, Sum, Avg, Prefetch
 
 # Create your views here.
 from .models import *
@@ -14,7 +14,11 @@ def user_posts(request, username):
     q = request.GET.get('qs')
     ex = request.GET.get('ex')
     # user = get_object_or_404(User, username=username)
-    all_posts = Post.objects.filter(owner__username=username).prefetch_related('comments').annotate(no_of_likes=Count('likes'))
+    all_posts = Post.objects.filter(owner__username=username).prefetch_related(Prefetch('comments', queryset=Comment.objects.filter(deleted=False).annotate(no_of_likes=Count('likes')), to_attr='all_comments')).annotate(no_of_likes=Count('likes'))
+    all_likes = all_posts.aggregate(all_likes=Sum('no_of_likes'))
+    print(all_likes)
+    # print(all_posts['all_likes'])
+
     if q:
         all_posts = all_posts.filter(content__icontains=q)
     if ex:
